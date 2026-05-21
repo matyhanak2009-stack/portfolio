b8cf67152f6927c6431417e4d1e16f43896ec543
Matyáš Hanák <matyhanak2009@gmail.com>
2026-05-21 23:11:00 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -25,7 +25,7 @@ controls.dampingFactor = 0.05;
 // 4. Načtení 3D modelu
 const loader = new GLTFLoader();
 loader.load(
-  './zemegula2.glb', // Kostka umístěná vedle index.html
+  './krabica.glb', // Kostka umístěná vedle index.html
   function (gltf) {
     const cube = gltf.scene;
     
